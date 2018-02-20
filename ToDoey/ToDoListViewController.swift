7c5fa7354ebac8baa73ca967b0c0de3f43ca5dc5
Craig Lester <three.story@gmail.com>
2018-02-20 15:55:00 -0500

    to do list complete with datasource and delegates

---
+++ ToDoey/ToDoListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Craig Lester on 2/20/18.
+//  Copyright © 2018 Craig Lester. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+let itemArray = ["Go Home", "get Groceries", "get AirBnB"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        
+    }
+
+   //Mark - Tableview Datasource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryView?.tintColor = UIColor.green
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+ 
+    
+}
+
