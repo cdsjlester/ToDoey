f261a25e5f069957ee663d4c8596b20c46f9edd5
Craig Lester <three.story@gmail.com>
2018-02-22 12:25:29 -0500

    added core data saving of data

--- ToDoey/ToDoListViewController.swift
+++ ToDoey/ToDoListViewController.swift
@@ -7,20 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
-class ToDoListViewController: UITableViewController {
-    var toDoItemToAdd = UITextField()
+class ToDoListViewController: UITableViewController  {
+    
+    
+    //var toDoItemToAdd = UITextField()
     //let defaults = UserDefaults.standard
+     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     //place to save data
-    let datafilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //let datafilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     var itemArray = [Item]()
+    var selectedCategory: Category?{
+        didSet{
+            
+            loadItemsFromDisk()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 //        let newItem = Item()
 //        newItem.title = "find Mike"
 //        itemArray.append(newItem)
-        loadItemsFromDisk()
+        
+      
+    
+        
         //if we want to use user defaults
 //        if let  items = defaults.array(forKey: "Item") as? [Item]{
 //            itemArray = items
@@ -49,7 +63,12 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //toggle checkmark
+       
+             //to delete from database
+       // context.delete(itemArray[indexPath.row])
+       // itemArray.remove(at: indexPath.row)
+        
+         //toggle checkmark
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveDataToDisk()
         tableView.cellForRow(at: indexPath)?.accessoryView?.tintColor = UIColor.green
@@ -58,26 +77,21 @@ class ToDoListViewController: UITableViewController {
     }
  
     @IBAction func addItem(_ sender: UIBarButtonItem) {
-       
+        var textfield = UITextField()
         let alert  = UIAlertController(title: "Add a New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             //button clicked on alert
-            if self.toDoItemToAdd.text != ""{
-                let data = Item()
-                data.title = self.toDoItemToAdd.text!
-                self.itemArray.append(data)
-                self.saveDataToDisk()
-                
-            }else{
-                //do something if no text
-                print (self.itemArray)
-                self.tableView.reloadData()
-            }
            
+            let newItem = Item(context: self.context )
+            newItem.title = textfield.text
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
+            self.itemArray.append(newItem)
+            self.saveDataToDisk()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = ("create new item")
-            self.toDoItemToAdd = alertTextField
+            textfield = alertTextField
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
@@ -86,28 +100,66 @@ class ToDoListViewController: UITableViewController {
     
     func saveDataToDisk(){
         //self.defaults.set(self.itemArray, forKey: "Item")
-        let encoder = PropertyListEncoder()
-        do{let saveData = try encoder.encode(itemArray)
-            try saveData.write(to: datafilePath!)}
-        catch{print(error)}
+        do{
+            try context.save()
+        }catch{
+            print("error saving ToDoList \(error)")
+        }
         tableView.reloadData()
     }
     
     
-    
-    func loadItemsFromDisk(){
-        if let data = try? Data(contentsOf: datafilePath!)
-        {
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-             
-            }catch{
-                print("could not load data!")
-            }
-            tableView.reloadData()
-       
+    //has a default value
+    func loadItemsFromDisk(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate : NSPredicate? = nil){
+        //let request : NSFetchRequest<Item> = Item.fetchRequest()
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate = predicate{
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,additionalPredicate])
+        }else{
+            request.predicate = categoryPredicate
         }
+        do{
+            itemArray = try context.fetch(request)
+        }catch{
+            
+        }
+        tableView.reloadData()
+    }
+    
+    
+   
+}
+//MARK: - Search Bar
+extension ToDoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        //%@ is a string
+    
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItemsFromDisk(with: request, predicate: request.predicate)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItemsFromDisk()
+            DispatchQueue.main.async {//use main queue for views
+                searchBar.resignFirstResponder() //clear keyboard and focus in searchbar
+            }
+        }        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
